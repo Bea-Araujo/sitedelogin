79a2dc41248f176faf53d3d0d44aa312b1477e5e
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-11-22 16:14:20 -0300

    path arrumado para sequelize em connection

--- back/connection.js
+++ back/connection.js
@@ -1,4 +1,4 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('mysql://root:mypass@localhost:/teste2')
+const sequelize = new Sequelize('mysql://root:mypass@localhost:3306/teste2')
 
 module.exports = sequelize;
